6609fdfa88270142329d6fde1c0ea48e42eaeef1
pecneb <pecneb@pop-os.localdomain>
2021-04-25 18:17:40 +0200

    skill rotation, add_heal, press_heal, start_rotation added

--- src/kbmodule.py
+++ src/kbmodule.py
@@ -8,16 +8,41 @@ class Klassz(object):
     nev = ""
     kepessegek = []
     skillrotation = []
+    heal = []
+    healtime = 0
 
     def __init__(self, nev):
         self.nev = nev
 
-    def skill_rotation(self, rotation, intervallumok):
+    # Sets the class skill rotation
+    def skill_rotation(self, rotation):
         for i in range(rotation):
             self.skillrotation.append(rotation[i])
-            self.skillrotation.append(intervallumok[i])
 
+    # Adds heal if the class has one
+    def add_heal(self, heals, healtime):
+        for i in range(heals):
+            self.heal.append(heals[i])
+        self.healtime = healtime
+    
+    # Heals with all the skills that given
+    def press_heal(self):
+        for i in range(self.heal):
+            keyboard.send(str(self.heal[i]))
 
+    # Starts the rotation
+    def start_rotation(self):
+        healtimer = 0
+        while True:
+            if(healtimer == self.healtime):
+                self.press_heal()
+            for i in range(self.skillrotation):
+                keyboard.send(str(i))
+                sleep(1.5)
+            healtimer += 1
+
+# Gives a little shell interface,
+# that lists all the classes the user can chose from.
 def interface(klasszok):
     print("Classok:")
     for i in range(klasszok):
@@ -25,12 +50,18 @@ def interface(klasszok):
     chosen = input("Klassz szama: ")
     return klasszok[chosen]
 
-def main(argv):
-    if len(argv) != 1:
-        print("Csak az eleresi utat ird be!")
-        sys.exit(1)
-    pathname = argv[0]
-    klasszok =[]
+
+# Opens the Game if needed (further implepentation is coming!)
+def open_aqw(PATH):
+    subprocess.Popen(PATH)
+
+
+def main():
+    Klasszok = []
+    Klasszok.append(Klassz("Infinite Legion Dark Caster"))
+    Klasszok[0].skill_rotation([4, 5, 3, 2], [1, 1, 1, 1])
+    Klasszok.append(Klassz("Blaze Binder"))
+    Klasszok[1].skill_rotation([2, 3, 5, 4], [1, 1, 1, 1])
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
