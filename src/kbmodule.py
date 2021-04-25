96d8849414b8d00f0903c1196068deea5d6d675f
pecneb <pecneb@pop-os.localdomain>
2021-04-25 20:40:50 +0200

    Ideiglenes megoldas, do_rotation def bevezetese classon kivul

--- src/kbmodule.py
+++ src/kbmodule.py
@@ -8,46 +8,41 @@ class Klassz(object):
     nev = ""
     kepessegek = []
     skillrotation = []
-    heal = []
+    heal = 0
     healtime = 0
 
-    def __init__(self, nev):
+    def __init__(self, nev, skillrotation, heal, healtime):
         self.nev = nev
-
-    # Sets the class skill rotation
-    def skill_rotation(self, rotation):
-        for i in range(rotation):
-            self.skillrotation.append(rotation[i])
-
-    # Adds heal if the class has one
-    def add_heal(self, heals, healtime):
-        for i in range(heals):
-            self.heal.append(heals[i])
+        self.skillrotation = skillrotation
+        self.heal = heal
         self.healtime = healtime
-    
-    # Heals with all the skills that given
-    def press_heal(self):
-        for i in range(self.heal):
-            keyboard.send(str(self.heal[i]))
 
-    # Starts the rotation
+    # Heals with all the skills that given (not working gotta fix)
+    def press_heal(self):
+        for heal in self.heal:
+            keyboard.send(str(heal))
+
+    # Starts the rotation (not working gotta fix)
     def start_rotation(self):
+        time.sleep(5)
         healtimer = 0
         while True:
             if(healtimer == self.healtime):
                 self.press_heal()
-            for i in range(self.skillrotation):
-                keyboard.send(str(i))
-                sleep(1.5)
+                healtimer = 0
+            for number in self.skillrotation:
+                keyboard.send(str(number))
+                time.sleep(1)     
             healtimer += 1
 
 # Gives a little shell interface,
 # that lists all the classes the user can chose from.
 def interface(klasszok):
     print("Classok:")
-    for i in range(klasszok):
-        print(f"{i+1}. {klasszok[i]}")
-    chosen = input("Klassz szama: ")
+    for i in range(len(klasszok)):
+        print(f"{i+1}. {klasszok[i].nev}")
+    chosen = int(input("Klassz szama: ")) - 1
+    print(f"Grind starts with {klasszok[chosen].nev}")
     return klasszok[chosen]
 
 
@@ -56,12 +51,27 @@ def open_aqw(PATH):
     subprocess.Popen(PATH)
 
 
+# Do rotation while the other on in the Class is not fixed
+def do_rotation(Klassz):
+    time.sleep(5)
+    healtimer = 0
+    while True:
+        if(healtimer == Klassz.healtime):
+            keyboard.send(str(Klassz.heal))
+            healtimer = 0
+            time.sleep(1)
+        for number in Klassz.skillrotation:
+            keyboard.send(str(number))
+            time.sleep(1)     
+        healtimer += 1
+
+
 def main():
     Klasszok = []
-    Klasszok.append(Klassz("Infinite Legion Dark Caster"))
-    Klasszok[0].skill_rotation([4, 5, 3, 2], [1, 1, 1, 1])
-    Klasszok.append(Klassz("Blaze Binder"))
-    Klasszok[1].skill_rotation([2, 3, 5, 4], [1, 1, 1, 1])
+    Klasszok.append(Klassz("Infinite Legion Dark Caster", [4, 5, 3], 2, 3))
+    Klasszok.append(Klassz("Blaze Binder", [2, 3, 5], 4, 2))
+    do_rotation(interface(Klasszok))
+
 
 if __name__ == "__main__":
     main()
